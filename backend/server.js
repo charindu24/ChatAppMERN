4c48f63718d0b95a2c23c2c150d32f6f4eca437d
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-12-08 22:05:10 +0530

    feat: setup dotenv, JSON parser, and mount auth/message routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.route.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
+
+// Middleware to parse incoming requests with JSON payloads
+app.use(express.json());  // This is necessary for parsing JSON request body
+
+// Mount the auth routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
 app.get("/", (req, res) => {
-    // Root route
-    res.send("Hello world!");
+  res.send("Hello world!");
 });
 
-// Mounting auth routes
-app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+  connectToMongoDB();  // Ensure MongoDB connection
+  console.log(`Server Running on port ${PORT}`);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+});
+
+
+
+
