2b0e749ffb99c039cdd1741e5e3b58bf2a97bb15
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-12-09 23:21:57 +0530

    chore: add cookie-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"; 
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -12,10 +14,12 @@ dotenv.config();
 
 // Middleware to parse incoming requests with JSON payloads
 app.use(express.json());  // This is necessary for parsing JSON request body
+app.use(cookieParser());
 
 // Mount the auth routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
 
 
 app.get("/", (req, res) => {
