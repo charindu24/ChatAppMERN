dd06d3e6cde0bdacddd8e4b8b490a946be119f7f
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-12-09 23:22:24 +0530

    refactor: destructure id from req.params and set senderId

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -4,7 +4,8 @@
 export const sendMessage = async(req, res) => {
     try {
         const {message} = req.body;
-        const id = req.params.id;
+        const {id} = req.params;
+        const senderId = req.userId;
         
     } catch (error) {
         console.log("Error in sendMessage controller: ", error.message)
