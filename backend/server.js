ff098affdc6271452470e9c3f8d7c97ba61d5df4
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-12-16 14:08:12 +0530

    import messageRoutes from "./routes/message.routes.js"; import userRoutes from "./routes/user.routes.js";

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; 
 
 import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
@@ -21,6 +23,7 @@ app.use(cookieParser());
 // Mount the auth routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 
