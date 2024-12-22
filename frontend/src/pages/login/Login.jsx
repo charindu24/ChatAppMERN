9c8c567254e0148cd20fff16cd0a83aad16d925f
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-12-22 22:09:48 +0530

    Ensure the form submission is handled properly for the login functionality

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,20 @@
+
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const{ loading, login } = useLogin()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-purple-400 bg-clip-padding backdrop-filter backdrop-blur-lg
@@ -10,19 +24,29 @@ const Login = () => {
           <span className="text-emerald-400"> VibeChat</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="textbase label-text">Username</span>
             </label>
-            <input type="text" placeholder="Enter username" className="w-full input input-bordered text-emerald-400 h-10" />
+            <input type="text" placeholder="Enter username" className="w-full input input-bordered text-emerald-400 h-10"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
 
           <div>
           <label className="label">
               <span className="textbase label-text">Password</span> 
             </label>
-            <input type="text" placeholder="Enter Password" className="w-full input input-bordered text-emerald-400 h-10" />
+            <input 
+            type='password'
+            placeholder="Enter Password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full input input-bordered text-emerald-400 h-10"
+              
+            />
             
             
           </div>
@@ -30,7 +54,13 @@ const Login = () => {
             {"Don't"} have an account?
           </Link>
           <div>
-          <button className="btn btn-block btn-success">Login</button>
+          <button 
+          className="btn btn-block btn-success"
+          disabled={loading}
+          >
+            {loading ? <span className="loading loading-spinner"></span> : 'Login'}
+            
+          </button>
           </div>
         </form>
 
