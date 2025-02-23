6db03615452d53cf2bee279018f3b58827206ad6
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-02-23 15:58:08 +0530

    const socket = io("http://localhost:5000",

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 
 const SocketContext = createContext();
 
-
+// eslint-disable-next-line react-refresh/only-export-components
 export const useSocketContext = () => {
 	return useContext(SocketContext);
 };
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
