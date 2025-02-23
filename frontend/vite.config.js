934b6f0ba707bed106922ec2ad7ac9db3de0a55f
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-02-23 21:22:58 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist", // Ensures Vite builds output to "dist" folder
+    emptyOutDir: true, // Clears old builds
+  },
 });
