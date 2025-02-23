3089ee9d8eafa4c246f9da471d123c93f6a7d5fc
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-02-23 15:57:20 +0530

    #

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,28 +7,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST"],
+	  origin: ["http://localhost:3000"],
+	  methods: ["GET", "POST"],
 	},
-});
+  });
+
+
+
+const userSocketMap = {}; // {userId: socketId}
 
 export const getReceiverSocketId = (receiverId) => {
 	return userSocketMap[receiverId];
 };
 
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
+  console.log("A user connected:", socket.id);
 
 	const userId = socket.handshake.query.userId;
 	if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-	// io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+	
+	io.emit("getOnlineUsers", Object.keys(userSocketMap));  // io.emit() is used to send events to all the connected clients
 
-	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
+	
+	socket.on("disconnect", () => { // socket.on() is used to listen to the events. can be used both on client and server side
 		console.log("user disconnected", socket.id);
 		delete userSocketMap[userId];
 		io.emit("getOnlineUsers", Object.keys(userSocketMap));
